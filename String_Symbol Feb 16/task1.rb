4f84897c0735cfe930bed70a7e73c26a781967d7
Smit Akhani <smit.akhani@bacancy.com>
2022-02-21 15:13:58 +0530

    string_symbol task completed

---
+++ String_Symbol Feb 16/task1.rb
@@ -0,0 +1,33 @@
+#chomp and chop method
+puts "hello\n".chomp
+puts "hello".chop
+
+#length and count method
+puts "hello".length
+puts "hello".count("l")
+
+#concat method
+puts "hello".concat(" world")
+
+#delete method
+puts "hello".delete("l")
+
+#delete_suffix and delete_prefix
+puts "hello world hello".delete_suffix("hello")
+puts "hello world hello".delete_prefix("hello")
+
+#capitalize method
+puts "hello world".capitalize
+
+#strip method
+puts " hello ".strip
+
+#reverse method
+puts "hello".reverse
+
+#upcase and downcase
+puts "Hello".upcase
+puts "Hello".downcase
+
+#swapcase method
+puts "Hello".swapcase
