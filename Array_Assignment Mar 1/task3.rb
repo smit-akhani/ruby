d7f77590c05c7760d7a7b930891da5d875087285
Smit Akhani <smit.akhani@bacancy.com>
2022-03-02 23:29:42 +0530

    array assignment

--- Array_Assignment Mar 1/task3.rb
+++ Array_Assignment Mar 1/task3.rb
@@ -8,12 +8,7 @@ Expected output: [0,1,2,3,4, nil, nil, nil, nil, nil]
 =end
 
 def removeDup(arr)
-    new_arr = []
-    arr.each do |ele|
-        if !new_arr.include?(ele)
-            new_arr.push(ele)
-        end    
-    end
+    new_arr = arr.uniq
     (arr.length-new_arr.length).times {|ele| new_arr.push(nil)}
     return new_arr
 end
