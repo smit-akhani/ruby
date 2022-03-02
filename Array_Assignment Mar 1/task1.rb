1b4f823799f87894440af0683eac27445a536b9a
Smit Akhani <smit.akhani@bacancy.com>
2022-03-02 21:31:33 +0530

    array assignment task 1

---
+++ Array_Assignment Mar 1/task1.rb
@@ -0,0 +1,22 @@
+=begin
+
+Given an array of numbers and a result, return indices of the two numbers such that they sum to the result.
+
+Input: numbers = [2,7,11,15], result = 13
+Expected output: [0,2]
+Explanation: numbers[0] + numbers[2] = 13
+
+=end
+
+num = [2,7,11,15]
+result = 13
+output_arr = []
+
+(num.length-1).times do |i|
+    (num.length-1).times do |j|
+        if num[i] + num[j+1] == result
+            output_arr.push(i,j+1)
+        end
+    end
+end
+print output_arr
---
+++ Array_Assignment Mar 1/task2.rb
@@ -0,0 +1,9 @@
+=begin
+
+Given an array of numbers and a result, return indices of the two numbers such that they sum to the result.
+
+Input: numbers = [2,7,11,15], result = 13
+Expected output: [0,2]
+Explanation: numbers[0] + numbers[2] = 13
+
+=end
